bc8ebbdfaf2ede70f5d4b7bce0071ebe0a7629c7
drmudgett <drmudgett@users.noreply.github.com>
2018-06-26 13:24:05 -0400

    Update variables.tf

--- infrastructure-as-code/k8s-cluster-openshift-aws/variables.tf
+++ infrastructure-as-code/k8s-cluster-openshift-aws/variables.tf
@@ -1,7 +1,7 @@
 //  The region we will deploy our cluster into.
 variable "region" {
   description = "Region to deploy the cluster into"
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "key_name" {
